c4dc4cebd0118d05e6676aff28233c02ef91e51f
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-04-24 16:41:30 -0400

    add email alert function and time loop

--- scraper.py
+++ scraper.py
@@ -1,35 +1,40 @@
 from playwright.sync_api import expect, sync_playwright
+import time
+from send_alert import send_email
 
-def detect_deal():
+def detect_deal(page):
     # look for BOGO deal
     deal_popup = page.get_by_role("dialog").filter(has_text="Buy 1, Get 1").or_(page.get_by_role("dialog").filter(has_text="PROMO CODE"))
     if (deal_popup.count() > 0):
-        print(deal_popup.text_content())
-        send_alert() 
+        body = deal_popup.text_content()
+        subject = "EAST TEA CAN LEGENDARY HUMMUS + CHICKEN BOGO! ON NOW!"
+        send_email(subject, body) 
     else:
         # alternatively, check if price has decreased
         menu_item = page.get_by_role("listitem").filter(has=page.get_by_role("heading", name ="Legendary Hummus + Chicken")).get_by_test_id("price-2c74bb69-ea49-4faf-99c9-e0fc70f86bcc")
-        price = menu_item.text_content()[1:] # remove dollar sign ($15.75)
+        price = menu_item.text_content().strip()[1:] # remove dollar sign ($15.75)
         if (float(price) < 15.75):
-            print(price)
-            send_alert()
+            subject = "EAST TEA CAN LEGENDARY HUMMUS + CHICKEN PRICE DROP!"
+            body = f"The online ordering price is currently ${price}!"
+            send_email(subject, body)
         else:
             print("no deal")
 
-with sync_playwright() as p:
-    browser = p.firefox.launch(headless=True)
-    page = browser.new_page()
-    page.goto("https://order.toasttab.com/online/east-tea-can-new-3115-winston-churchill-blvd-unit-1")
-    # page.wait_for_load_state("networkidle")
+while True:
+    try:
+        with sync_playwright() as p:
+            browser = p.firefox.launch(headless=True)
+            page = browser.new_page()
+            page.goto("https://order.toasttab.com/online/east-tea-can-new-3115-winston-churchill-blvd-unit-1")
+            page.wait_for_load_state("domcontentloaded")
+            time.sleep(3)
+            expect(page.locator("h2").nth(0)).to_contain_text("East Tea Can Mississauga") # checks that we're seeing expected info
+            print(page.locator("h2").nth(0).text_content())
 
-    expect(page.locator("h2").nth(0)).to_contain_text("East Tea Can Mississauga") # checks that we're seeing expected info
-    print(page.locator("h2").nth(0).text_content())
+            detect_deal(page)
+            browser.close()
+    except Exception as err:
+        subject = "Hummus Bot Error"
+        send_email(subject, str(err))
 
-    detect_deal()
-    browser.close()
-
-
-
-def send_alert():
-    pass
-
+    time.sleep(48*60*60) # delay --> run program every 2 days 
