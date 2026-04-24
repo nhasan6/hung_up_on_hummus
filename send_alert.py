edfd9b8e94e3a3627d96357ff3fd4c3236f67dba
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-04-24 18:07:45 -0400

    differentiate btwn sender and recipient

--- send_alert.py
+++ send_alert.py
@@ -7,18 +7,20 @@ from dotenv import load_dotenv
 load_dotenv(Path(__file__).with_name(".env"))
 
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
+RECIPIENT_EMAIL_ADDRESS = os.getenv("RECIPIENT_EMAIL_ADDRESS")
+
 
 def send_email(subject: str, body: str):
     msg = EmailMessage()
     msg["Subject"] = subject
-    msg["From"] = EMAIL_ADDRESS
-    msg["To"] = EMAIL_ADDRESS
+    msg["From"] = SENDER_EMAIL_ADDRESS
+    msg["To"] = RECIPIENT_EMAIL_ADDRESS
     msg.set_content(body)
 
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
         smtp.starttls()
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.login(SENDER_EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
         print("Success!")
         
